ff37ff535dbfb8d382ae5ea4071a4f0269105cec
Domenico Visconti <domenico.visconti819@gmail.com>
2018-12-12 20:25:49 +0100

    reformat code

--- src/rss_feed_reader/models/auto_complete.clj
+++ src/rss_feed_reader/models/auto_complete.clj
@@ -3,22 +3,20 @@
 (defn autocomplete-id
   "set entity id if missing"
   [subscription]
-
-  (if (not (:id subscription))
+  (if-not (:id subscription)
     (assoc subscription :id (java.util.UUID/randomUUID))
     subscription))
 
 (defn autocomplete-insert-date
   "set entity insert-date if missing"
   [subscription]
-
-  (if (not (:insert_date subscription))
+  (if-not (:insert_date subscription)
     (assoc subscription :insert_date (java.sql.Timestamp/from (java.time.Instant/now)))
     subscription))
 
 (defn autocomplete-version
   "set entity version if missing"
   [subscription]
-  (if (not (:version subscription))
+  (if-not (:version subscription)
     (assoc subscription :version 0)
     subscription))
