789a78d00380a6a50bde766c22b4726c5e996722
Domenico Visconti <domenico.visconti819@gmail.com>
2019-06-10 08:40:25 +0200

    avoid to disable feed on feed fetching error

--- src/rss_feed_reader/jobs/rss_fetching_job.clj
+++ src/rss_feed_reader/jobs/rss_fetching_job.clj
@@ -3,15 +3,24 @@
             [clojure.core.async :refer [go]]
             [rss-feed-reader.model.feed-item-model :as feed-item]
             [rss-feed-reader.model.feed-model :as feed]
-            [rss-feed-reader.rss.apis :as rss]))
+            [rss-feed-reader.rss.apis :as rss]
+            [clojure.tools.logging :as log]))
 
-(defn fetch-all-feeds
-  []
-  (let [feeds (feed/all-enabled)]
+(defn- fetch-feeds-async
+  [feeds]
+  (go
     (doseq [feed feeds
-            feed-item (rss/fetch-feed-or-disable feed)]
+            feed-item (rss/fetch-feed feed)]
       (let [hash (rss/get-feed-item-hash feed-item)]
         (when-not (feed-item/by-hash hash)
           (feed-item/insert {:feed-id (:id feed)
                              :item    (generate-string feed-item)
                              :hash    hash}))))))
+
+(defn fetch-all-feeds
+  []
+  (let [feeds (feed/all-enabled)]
+    (try
+      (fetch-feeds-async feeds)
+      (catch Exception e
+        (log/error "error fetching feeds=" feeds " , error=" e)))))
